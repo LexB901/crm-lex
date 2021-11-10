747bf3a8d8483bccf6874ec3465ebdabb86c935b
Lex <39721295+DutchPrince@users.noreply.github.com>
2021-11-10 11:44:49 +0100

    dsa

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -130,7 +130,7 @@
                     M202.667,362.669c-88.235,0-160-71.765-160-160s71.765-160,160-160s160,71.765,160,160S290.901,362.669,202.667,362.669z">
                 </path>
             </svg>
-            <a href="https://crm.flirtserver.xyz/dashboard">
+            <a href="">
                 <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="backward" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="headerbackimg">
                     <path fill="currentColor" d="M459.5 71.41l-171.5 142.9v83.45l171.5 142.9C480.1 457.7 512 443.3 512 415.1V96.03C512 68.66 480.1 54.28 459.5 71.41zM203.5 71.41L11.44 231.4c-15.25 12.87-15.25 36.37 0 49.24l192 159.1c20.63 17.12 52.51 2.749 52.51-24.62v-319.9C255.1 68.66 224.1 54.28 203.5 71.41z" class=""></path>
                 </svg>
--- resources/views/expense/components/footer.blade.php
+++
@@ -1,5 +0,0 @@
-<footer>
-    <div class="footer">
-        <p class="pfooter">Copyright © 2021 Weetjes TM, Inc.</p>
-    </div>
-</footer>
